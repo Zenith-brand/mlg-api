9273d17c9a85b1813001a71e014ee32e052fd8f8
YURI SOROKINS <yuri@gudideas.co.uk>
2023-01-05 19:38:19 +0000

    Add user status

---
+++ database/seeders/User_statusesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User_status;
+
+class User_statusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        User_status::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few User_statuss in our database:
+        for ($i = 0; $i < 10; $i++) {
+            User_status::create([
+                // 'status' => $faker->name,
+                'status' => 'status',
+            ]);
+        }
+    }
+}
