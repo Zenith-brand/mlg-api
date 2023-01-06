81ca33b7a57e82791ab27fd122cc50a915b5fa3b
YURI SOROKINS <yuri@gudideas.co.uk>
2023-01-06 18:09:04 +0000

    Check for access token in DB

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -7,6 +7,9 @@ use JWTAuth;
 use Exception;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
+use App\Models\Access_token;
+use Illuminate\Support\Facades\DB;
+
 class JwtMiddleware extends BaseMiddleware
 {
 
@@ -19,17 +22,50 @@ class JwtMiddleware extends BaseMiddleware
      */
     public function handle($request, Closure $next)
     {
-        try {
-            $user = JWTAuth::parseToken()->authenticate();
-        } catch (Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
-            }else{
-                return response()->json(['status' => 'Authorization Token not found']);
-            }
+        // convert from string to json object
+        $access_token = json_decode($request->getContent())->access_token;
+
+        // initialize an empty array to store tokens
+        $tokens = [];
+
+        // search for tokens in DB and add them to the array
+        $db_tokens = DB::select('select token from access_tokens');
+        foreach ($db_tokens as $token) {
+            array_push($tokens, $token->token);
         }
-        return $next($request);
+
+        // if provided access token exist in DB allow access to the midlleware
+        if (in_array($access_token, $tokens)){
+            // dd($access_token == $password, "here", (int)$password, $password);
+            try {
+                $user = JWTAuth::parseToken()->authenticate();
+            } catch (Exception $e) {
+                if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                    return response()->json(['status' => 'Token is Invalid']);
+                }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                    return response()->json(['status' => 'Token is Expired']);
+                }else{
+                    return response()->json(['status' => 'Authorization Token not found']);
+                }
+            }
+            return $next($request);
+        // return an error if token is invalid 
+        } else {
+            return response()->json(['status code' => 401,'message' => 'Access Token not found or invalid']);
+        }
+        // 
+
+        // try {
+        //     $user = JWTAuth::parseToken()->authenticate();
+        // } catch (Exception $e) {
+        //     if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+        //         return response()->json(['status' => 'Token is Invalid']);
+        //     }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+        //         return response()->json(['status' => 'Token is Expired']);
+        //     }else{
+        //         return response()->json(['status' => 'Authorization Token not found']);
+        //     }
+        // }
+        // return $next($request);
     }
 }
