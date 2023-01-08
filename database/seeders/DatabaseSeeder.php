4be4ef4aa6fa732317f9dc4ad6f18b751a1d359b
Yuri Sorokins <93756242+yuridevweb@users.noreply.github.com>
2023-01-08 12:19:05 +0000

    Run seeds from one place

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Access_token;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         $this->call([
+            UsersTableSeeder::class,
+            User_statusesTableSeeder::class,
+            ClientsTableSeeder::class,
+            Access_tokensTableSeeder::class
+        ]);
     }
 }
